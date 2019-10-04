3af2332a45677cc367a694bb04114c27d2fa159a
Casey Webb <notcaseywebb@gmail.com>
2019-10-03 21:08:10 -0500

    chore: update testing deps and limit test scope

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function(config) {
   config.set({
     basePath: "",
     frameworks: ["jasmine"],
-    files: ["dist/umd/*.js", "test/util/*.js", "test/**/*Spec.js"],
+    files: ["dist/umd/scrollspy.js", "test/util/*.js", "test/**/*Spec.js"],
     reporters: ["progress"],
     port: 9876,
     colors: true,
